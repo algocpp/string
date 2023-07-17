130109b5bd1c20127f41e09c689755337f9fcd4d
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-07-17 11:38:04 +0900

    `std::string`の掛け算

---
+++ multiplication.hpp
@@ -0,0 +1,29 @@
+
+// algocpp/string/multiplication.hpp
+//
+// This file is part of algocpp and is copyrighted by algocpp.
+// If used, it must comply with the MIT License.
+
+#ifndef ALGOCPP_STRING_MULTIPLIATION
+#define ALGOCPP_STRING_MULTIPLIATION
+
+#include <string>
+
+inline std::string operator*(std::string const &s, unsigned long long x)
+{
+	std::string ans = "";
+	while (x--)
+	{
+		ans += s;
+	}
+
+	return ans;
+}
+
+inline std::string operator*=(std::string &s, unsigned long long x)
+{
+	s = s * x;
+	return s;
+}
+
+#endif // ALGOCPP_STRING_MULTIPLIATION

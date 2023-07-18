7b2e9e59d36460c923a6072a3b8768d0c161d509
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-07-18 19:08:00 +0900

    回文判定

---
+++ is_palind.hpp
@@ -0,0 +1,32 @@
+
+// algocpp/string/is_palind.hpp
+//
+// This file is part of algocpp and is copyrighted by algocpp.
+// If used, it must comply with the MIT License.
+
+#ifndef ALGOCPP_STRING_ISPALIND
+#define ALGOCPP_STRING_ISPALIND
+
+#include <string>
+#include <locale>
+
+namespace algocpp
+{
+	namespace string
+	{
+		inline bool is_palind(std::string s)
+		{
+			for (unsigned long long i = 0; i < (unsigned long long)(s.size() / 2); i++)
+			{
+				if (s[i] != s[s.size() - i - 1])
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+	}
+}
+
+#endif // ALGOCPP_STRING_ISPALIND

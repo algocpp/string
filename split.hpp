01289287b945c1862fc24d73dda3a899e68b63e4
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-07-21 21:26:22 +0900

    様々な型に対応

--- split.hpp
+++ split.hpp
@@ -9,54 +9,95 @@
 
 #include <vector>
 #include <string>
-#include <assert.h>
+#include <locale>
 #include <algocpp/exception/blank.hpp>
+#include <algocpp/string/wconvert.hpp>
 
 namespace algocpp
 {
 	namespace string
 	{
-		inline std::vector<std::string> split(std::string s, std::string div)
+		namespace base
 		{
-			if (div == "")
+			template <typename Tp1>
+			inline std::vector<Tp1> base_split(Tp1 s, Tp1 div)
 			{
-				throw algocpp::exception::blank_argument("It cannot be split by an empty string.");
-			}
+				Tp1 blank = Tp1{};
 
-			std::vector<std::string> result;
-			std::string tmp = "";
-			for (unsigned long long i = 0; i < s.size(); i++)
-			{
-				if (s.size() - i >= div.size() && s[i] == div[0])
+				if (div == blank)
 				{
-					bool flag = true;
-					for (unsigned long long j = 0; j < div.size(); j++)
+					std::vector<Tp1> result(s.size());
+
+					for (int i = 0; i < s.size(); i++)
 					{
-						if (s[i + j] != div[j])
+						result[i] = Tp1{s[i]};
+					}
+
+					return result;
+				}
+
+				std::vector<Tp1> result;
+				Tp1 tmp = blank;
+				for (unsigned long long i = 0; i < s.size(); i++)
+				{
+					if (s.size() - i >= div.size() && s[i] == div[0])
+					{
+						bool flag = true;
+						for (unsigned long long j = 0; j < div.size(); j++)
 						{
-							flag = false;
-							break;
+							if (s[i + j] != div[j])
+							{
+								flag = false;
+								break;
+							}
+						}
+
+						if (flag)
+						{
+							result.push_back(tmp);
+							tmp = blank;
+							i += div.size() - 1;
 						}
 					}
-
-					if (flag && tmp != "")
+					else
 					{
-						result.push_back(tmp);
-						tmp = "";
-						i += div.size() - 1;
+						tmp += s[i];
 					}
 				}
-				else
-				{
-					tmp += s[i];
-				}
-			}
 
-			if (tmp != "")
+				// if (tmp != blank)
 				result.push_back(tmp);
 
-			return result;
+				return result;
+			}
 		}
+
+		inline std::vector<std::string> split(std::string s, std::string div)
+		{
+			return base::base_split<std::string>(s, div);
+		}
+
+		inline std::vector<std::u32string> split(std::u32string s, std::u32string div)
+		{
+			return base::base_split<std::u32string>(s, div);
+		}
+
+		inline std::vector<std::u16string> split(std::u16string s, std::u16string div)
+		{
+			return base::base_split<std::u16string>(s, div);
+		}
+
+		inline std::vector<std::wstring> split(std::wstring s, std::wstring div)
+		{
+			return base::base_split<std::wstring>(s, div);
+		}
+
+#if __cplusplus >= 202002LL
+		inline std::vector<std::u8string> split(std::u8string s, std::u8string div)
+		{
+			return base::base_split<std::u8string>(s, div);
+		}
+#endif
 	}
 }
 
